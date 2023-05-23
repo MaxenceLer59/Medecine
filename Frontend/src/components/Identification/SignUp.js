b2addf7baa62be5c599864340e3f4a2384868d05
Maxence Leroy <maxence.leroy@epitech.eu>
2023-05-23 15:30:34 +0200

    Add a user to the firebase realtime database : OK

--- Frontend/src/components/Identification/SignUp.js
+++ Frontend/src/components/Identification/SignUp.js
@@ -1,80 +1,109 @@
 import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
-import { auth } from "../../../../config/firebase-config";
+import { auth, db } from "../../../../config/firebase-config";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { ref, set } from "firebase/database";
 import "../../styles/Identification.scss";
 
 const SignUp = ({ switch_identification }) => {
+  //States
+  const [isHovered, setIsHovered] = useState(false);
+  const [userSignup, setUserSignup] = useState({
+    user_email: "",
+    user_password: "",
+    user_confirmPassword: "",
+  });
 
-    const registerRoute = ENDPOINTS.USER_SIGNUP
-    //States
-    const [isHovered, setIsHovered] = useState(false);
-    const [userSignup, setUserSignup] = useState({
-        user_email: "",
-        user_password: "",
-        user_confirmPassword: "",
-    })
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
 
-    const toastOptions = {
-        position: "bottom-right",
-        autoClose: 8000,
-        pauseOnHover: true,
-        draggable: true,
-        theme: "dark",
-    };
+  const signup = async (e) => {
+    try {
+      e.preventDefault();
+      const { user_email, user_password } = userSignup;
+      createUserWithEmailAndPassword(auth, user_email, user_password)
+        .then((userCredential) => {
+          // Signed in
+          const userId = userCredential.user.uid;
+          set(ref(db, "users/" + userId), {
+            user_email: user_email,
+            user_password: user_password,
+          });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+        });
+    } catch (err) {
+      throw err;
+    }
+  };
 
-
-    const signup = async (e) => {
-        try {
-            e.preventDefault();
-            const { user_email, user_password } = userSignup;
-            createUserWithEmailAndPassword(auth, user_email, user_password);
-        } catch (err) {
-            throw err;
-        }
-    };
-
-
-    return (
-        <div className="identification-container">
-            <form className="identification-form" onSubmit={signup}>
-                <h2 className="identification-form-title">S'inscrire</h2>
-                <input
-                    className="identification-input"
-                    type="email"
-                    placeholder="Adresse e-mail"
-                    onChange={(e) => {
-                        setUserSignup({
-                            ...userSignup,
-                            user_email: e.target.value,
-                        })
-                    }} />
-                <input
-                    className="identification-input"
-                    type="password"
-                    placeholder="Mot de passe"
-                    onChange={(e) => {
-                        setUserSignup({
-                            ...userSignup,
-                            user_password: e.target.value,
-                        })
-                    }} />
-                <input
-                    className="identification-input"
-                    type="password"
-                    placeholder="Confirmer le mot de passe"
-                    onChange={(e) => {
-                        setUserSignup({
-                            ...userSignup,
-                            user_confirmPassword: e.target.value,
-                        })
-                    }} />
-                <button className={isHovered ? 'identification-button-hovered' : 'identification-button'} type="submit" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>S'inscrire</button>
-                <button className="identification-link-button" onClick={switch_identification}>Déjà un compte ?</button>
-            </form>
-            <ToastContainer />
-        </div>
-    )
+  return (
+    <div className="identification-container">
+      <form className="identification-form" onSubmit={signup}>
+        <h2 className="identification-form-title">S'inscrire</h2>
+        <input
+          className="identification-input"
+          type="email"
+          placeholder="Adresse e-mail"
+          onChange={(e) => {
+            setUserSignup({
+              ...userSignup,
+              user_email: e.target.value,
+            });
+          }}
+        />
+        <input
+          className="identification-input"
+          type="password"
+          placeholder="Mot de passe"
+          onChange={(e) => {
+            setUserSignup({
+              ...userSignup,
+              user_password: e.target.value,
+            });
+          }}
+        />
+        <input
+          className="identification-input"
+          type="password"
+          placeholder="Confirmer le mot de passe"
+          onChange={(e) => {
+            setUserSignup({
+              ...userSignup,
+              user_confirmPassword: e.target.value,
+            });
+          }}
+        />
+        <button
+          className={
+            isHovered
+              ? "identification-button-hovered"
+              : "identification-button"
+          }
+          type="submit"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          S'inscrire
+        </button>
+        <button
+          className="identification-link-button"
+          onClick={switch_identification}
+        >
+          Déjà un compte ?
+        </button>
+      </form>
+      <ToastContainer />
+    </div>
+  );
 };
 
 export default SignUp;
