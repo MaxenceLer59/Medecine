f5d02bff16d7fbf2b0ca9d660a597f107c94f6ad
Maxence Leroy <maxence.leroy@epitech.eu>
2023-06-02 09:36:02 +0200

    Patients - Make List and Add search bar

--- Frontend/src/components/Patients/Patients.js
+++ Frontend/src/components/Patients/Patients.js
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { onValue, ref, getDatabase } from "firebase/database";
+import { db } from "../../../../config/firebase-config";
+import { UserContext } from "../../../../config/userContext";
+import "../../styles/Patients.scss";
+import people from "../../../public/Image/people-logo.png"
+import loop from "../../../public/Image/loop.png";
 
 const Patients = () => {
+    const [patientList, setPatientList] = useState([]);
+    const userUID = useContext(UserContext).userUID;
+
+    useEffect(() => {
+        const database = getDatabase();
+
+        const patientsRef = ref(database, "patients");
+
+        onValue(patientsRef, (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+                const patients = Object.values(data);
+                setPatientList(patients);
+            }
+        });
+    }, []);
+
     return (
-        <h1>Patients</h1>
-    )
-}
+        <div className="patients-container">
+            <form>
+                <input type="text" placeholder="Rechercher un patient"/>
+                <span className="separate"/>
+                <img src={loop} alt="loop"/>
+            </form>
+            {patientList.map((patient) => {
+                // Vérification si userUID correspond à user.uid
+                if (patient.user_uid === userUID) {
+                    return (
+                        <div className="patient-card">
+                            <img src={people} alt="people" />
+                            <p>
+                                <span className="toUpperCase">{patient.patient_name} </span> 
+                                {patient.patient_firstname}
+                            </p>
+                        </div>
+                    );
+                } else {
+                    return null; // Ignorer le patient s'il ne correspond pas à l'utilisateur actuel
+                }
+            })}
+        </div>
+    );
+};
+
 export default Patients;
